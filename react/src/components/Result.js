058c6dc48285ad823d0098dd8672f631d22ce9bb
notrowing <notrowing@naver.com>
2024-01-24 18:03:32 +0900

    결과 css 1차 수정

--- react/src/components/Result.js
+++ react/src/components/Result.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "../static/css/Result.css";
-
+import Footer from "./Footer";
 function Result() {
+
+    function btnClick(e){
+        window.location.href="/Input"
+    }
+
     const [wat, setWat] = useState('');
     const [subImg1, setSubImage1] = useState('');
     const [subImg2, setSubImage2] = useState('');
@@ -29,64 +34,65 @@ function Result() {
 
     return (
         <div className="result_component">
-            <div className="logo_container">
-                <a href="/">
-                    <div className="logo"/>
-                </a>
-            </div>
+            <section>
 
-            <div className="modal_container">
-                <div className="wrap_container">
-                    <div className="image_container">
+               <div className="show_Image">
+                    <div className="mainImage_box">
                         <img src={img} className="main_image"/>
-
-                        <div className="sub_container">
-                            <div className="sub_text">※ 학습 결과와 유사한 데이터</div>
-
-                            <div className="sub_image">
-                                <img src={subImg1} className="sub_image1"/>
-                                <img src={subImg2} className="sub_image2"/>
-                                <img src={subImg3} className="sub_image3"/>
-                                <img src={subImg4} className="sub_image4"/>
-                            </div>
-                        </div>
                     </div>
+               </div>
 
-                    <div className="result_container">
-                        <div className="result_energy">
-                            <div className="result_energy_top">
-                                해당 사진의 일사량은
+                <div className="modal_container">
+                    <div className="wrap_container">
+                            <div className="sub_container">
+                                <div className="sub_text">학습 결과와 유사한 데이터는 다음과 같아요</div>
+
+                                <div className="sub_image">
+                                    <img src={subImg1} className="sub_image1"/>
+                                    <img src={subImg2} className="sub_image2"/>
+                                    <img src={subImg3} className="sub_image3"/>
+                                    <img src={subImg4} className="sub_image4"/>
+                                </div>
+                            </div>
+                      
+
+                        <div className="result_container">
+                            <div className="result_energy">
+                                <div className="result_energy_top">
+                                    해당 사진의 일사량은
+                                </div>
+
+                                <div className="result_energy_middle">
+                                {wat_low || '0'} ~ {wat_high || '0'} W/m²
+                                </div>
+
+                                <div className="result_energy_bottom">
+                                    로 예상됩니다
+                                </div>
                             </div>
 
-                            <div className="result_energy_middle">
-                            {wat_low || '0'} ~ {wat_high || '0'} W/m²
+                            <div className="use_energy">
+                                <div className="use_energy_top">
+                                    충전 가능한 건전지 개수는
+                                </div>
+
+                                <div className="use_energy_middle">
+                                {battery_low || '0'} ~ {battery_high || '0'}
+                                </div>
+
+                                <div className="use_energy_bottom">
+                                    개 입니다
+                                </div>
                             </div>
 
-                            <div className="result_energy_bottom">
-                                로 예상됩니다
-                            </div>
                         </div>
 
-                        <div className="use_energy">
-                            <div className="use_energy_top">
-                                충전 가능한 건전지 개수는
-                            </div>
+                        <button className="reset" onClick={btnClick}>Reset</button>
 
-                            <div className="use_energy_middle">
-                            {battery_low || '0'} ~ {battery_high || '0'}
-                            </div>
-
-                            <div className="use_energy_bottom">
-                                개 입니다
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="reset">
-                        <a href="/Input" className="reset_button">Reset</a>
                     </div>
                 </div>
-            </div>
+            </section>
+            <Footer/>
         </div>
     )
 }
