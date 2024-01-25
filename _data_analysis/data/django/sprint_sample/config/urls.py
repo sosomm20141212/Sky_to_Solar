2feaf5f231dd4f28f7ee4595f814fe66d54b8359
sosomm20141212 <sosomm20141212@gmail.com>
2024-01-26 01:58:45 +0900

    최초의 장고 파일 삭제

--- _data_analysis/data/django/sprint_sample/config/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from predict.views import model_predict_view  
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', model_predict_view, name='model_predict'),  
-]
